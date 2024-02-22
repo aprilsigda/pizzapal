b17e9bc667e8cb718c1e93eeac7047a6c88e2394
April Sigda <aprilsigda@gmail.com>
2024-02-22 05:59:16 -0700

    Updated database.py documentation

--- src/database.py
+++ src/database.py
@@ -1,7 +1,23 @@
+'''
+File: database.py
+Author: April Sigda
+Purpose: Sets up the connection to the database backend
+'''
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Initializes the database connection. If the
+# necessary tables are not already present,
+# creates them. This should be called every
+# time the app is run.
+# Parameters:
+# - app (Flask): the Flask object representing
+#       the currently running application
+# Requires:
+# - app.config['SECRET_KEY']
+# - app.config['SQLALCHEMY_DATABASE_URI']
+# (these are set up in __init__.py)
 def init_db(app):
     db.init_app(app)
     from . import models
