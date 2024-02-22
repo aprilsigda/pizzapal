6e8b2b7b7f149fefa5d71ea886bfe33aa07c21e2
April Sigda <aprilsigda@gmail.com>
2024-02-22 09:16:22 -0700

    Renamed 'src' folder to 'app' for easier running

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,5 +1,5 @@
 import azure.functions as func
-from src import app
+from app import app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
