4f34d49b3956f4fd327453b2a2420e2bbe401e9b
April Sigda <aprilsigda@gmail.com>
2024-02-22 05:36:35 -0700

    Updated settings.py documentation

--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,7 @@
+'''
+Insert the connection string URI for the database backend here.
+Default value is 'sqlite:///db.sqlite', which uses a locally stored
+database file. Any SQL based database should work with minimal
+modifications to the program, if any.
+'''
 DATABASE_CONNECTION_STRING = 'sqlite:///db.sqlite'
