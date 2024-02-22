70d002cba8d935c65ad7ada35db474388dec014f
April Sigda <aprilsigda@gmail.com>
2024-02-22 05:48:38 -0700

    Updated pages.py documentation

--- src/pages.py
+++ src/pages.py
@@ -1,3 +1,8 @@
+'''
+File: pages.py
+Author: April Sigda
+Purpose: Routes page requests to the proper locations
+'''
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 from . import models
@@ -6,10 +11,15 @@ from .topping import *
 
 pages = Blueprint('pages', __name__)
 
+# Redirects all requests for the root page to the homepage
 @pages.route('/')
 def index():
     return redirect(url_for('pages.home'))
 
+# Routes requests to the '/home' endpoint.
+# - if there is no user logged in: redirect to the login page
+# - if the logged in user is a chef: display the pizza management page
+# - if the logged in user is a manager: display the topping management page
 @pages.route('/home')
 def home():
     if not current_user.is_authenticated:
